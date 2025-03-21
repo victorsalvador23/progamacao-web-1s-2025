bd0e70c43ee52f79969c6cb90cab486217468b4d
victorsalvador23 <victorsaldias2005-@gmail.com>
2025-03-20 22:34:24 -0300

    teste

--- atividade3/index.js
+++ atividade3/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const calc = require('./calculadora');
 const app = express();
 
-app.get('/ola', (req, res)=>{
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à minha API!');
+});
+
+app.get('/ola', (req, res) => {
     res.send('Hello, world!');
 });
 
-
-app.get('/ola/:nome', (req, res)=>{
+app.get('/ola/:nome', (req, res) => {
     res.send('Olá, ' + req.params.nome);
 });
 
-
 const PORT = 8080;
-app.listen (PORT, ()=>{
-    console.log('app rodando na porta' + PORT);
+app.listen(PORT, () => {
+    console.log('App rodando na porta ' + PORT);
 });
